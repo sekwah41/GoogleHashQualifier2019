a2b4638816a611f1c1eb06c36196a4647eac94e2
James Lewis <james.lewis@jhlws.com>
2019-02-28 19:03:14 +0000

    Compute score of a slide, update slide validation.

--- src/main/java/swndve/Slide.java
+++ src/main/java/swndve/Slide.java
@@ -1,6 +1,8 @@
 package swndve;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
@@ -9,20 +11,23 @@ public class Slide {
   private final SortedSet<String> tags;
   private final List<Image> images;
 
-  public Slide(List<Image> images) throws Exception {
+  public Slide(List<Image> images) {
     this.images = images;
-    if (images.size() == 2) {
-      for (Image p : images) {
-        if (!p.isVertical()) {
-          throw new Exception("Invalid image combination");
+    switch (images.size()) {
+      case 1:
+        if (images.stream().allMatch(Image::isHorizontal)) {
+          throw new IllegalArgumentException("Single image slides should be horizontal.");
         }
-      }
-
-    } else if (images.size() == 1) {
-      if (images.get(0).isVertical()) {
-        throw new Exception("Invalid image combination");
-      }
+        break;
+      case 2:
+        if (images.stream().allMatch(Image::isVertical)) {
+          throw new IllegalArgumentException("Multi image slides should be vertical.");
+        }
+        break;
+      default:
+        throw new IllegalArgumentException("Slides must have one or two images.");
     }
+
 
     this.tags = new TreeSet<>();
     for (Image image : images) {
@@ -36,5 +41,21 @@ public class Slide {
 
   public List<Image> getImages() {
     return images;
+  }
+
+  public static int computeScore(Set<String> set1, Set<String> set2) {
+    Set<String> union = new HashSet<>(set1);
+    union.addAll(set2);
+
+    Set<String> intersection = new HashSet<>(set1);
+    intersection.retainAll(set2);
+
+    Set<String> differenceS1 = new HashSet<>(set1);
+    differenceS1.removeAll(set2);
+
+    Set<String> differenceS2 = new HashSet<>(set2);
+    differenceS2.removeAll(set1);
+
+    return (Math.min(intersection.size(), Math.min(differenceS1.size(), differenceS2.size())));
   }
 }
