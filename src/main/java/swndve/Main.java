b5f522a5c3c1dc053fe630590eca32f8809cbac1
James Lewis <james.lewis@jhlws.com>
2019-02-28 17:27:02 +0000

    Add some pointless timers to test environment is working.

--- src/main/java/swndve/Main.java
+++ src/main/java/swndve/Main.java
@@ -2,11 +2,14 @@ package swndve;
 
 import static java.util.logging.Level.INFO;
 
+import com.google.common.base.Stopwatch;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
 public class Main {
 
   private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
+  private static final Stopwatch mainStopwatch = Stopwatch.createUnstarted();
   private static final String[] INPUT_FILES = {};
 
   public static void main(String[] args) {
@@ -15,7 +18,14 @@ public class Main {
     LOGGER.log(INFO, "Started application");
 
     for (String fileName : INPUT_FILES) {
+      mainStopwatch.reset().start();
       LOGGER.log(INFO, String.format("Processing File: %s", fileName));
+
+      LOGGER.log(
+          INFO,
+          String.format(
+              "Processed File: %s in %d seconds",
+              fileName, mainStopwatch.elapsed(TimeUnit.SECONDS)));
     }
   }
 }
