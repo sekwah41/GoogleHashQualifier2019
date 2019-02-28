e31b4bc91c15155f8b0dc5bce5802075ab2525ab
ArmedXWings <adithya123@hotmail.co.uk>
2019-02-28 18:08:25 +0000

    Added Scoring static method

--- src/main/java/swndve/Problem.java
+++ src/main/java/swndve/Problem.java
@@ -6,49 +6,60 @@ import com.google.common.base.Stopwatch;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.SortedSet;
-import java.util.TreeSet;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
 public class Problem {
 
-  private final Logger LOGGER = Logger.getLogger(Problem.class.getName());
-  private final Stopwatch problemStopwatch = Stopwatch.createUnstarted();
-
-  private int imageCount;
-  private List<Image> images;
-  private SortedSet<String> tags;
+  private static final Logger LOGGER = Logger.getLogger(Problem.class.getName());
+  private static final Stopwatch problemStopwatch = Stopwatch.createUnstarted();
 
   public Problem() {
   }
 
   public Problem(String fileName) {
 
-    String line;
+    String line = null;
     try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
       problemStopwatch.reset().start();
       line = reader.readLine();
       String[] arguments = line.split(" ");
-      imageCount = Integer.parseInt(arguments[0]);
-      images = new ArrayList<>(imageCount);
-      tags = new TreeSet<>();
+      // TODO: Parse whole problem arguments here
+
       int idx = 0;
       while ((line = reader.readLine()) != null) {
-        Image image = Image.fromString(idx++, line);
-        tags.addAll(image.getTags());
-        images.add(image);
+        arguments = line.split(" ");
+        // TODO: parse individual lines here
+
+        idx++;
       }
 
+      // TODO: Log problem details here
       LOGGER.log(
           INFO,
-          String.format(
-              "Problem created in %d seconds with arguments: Images %d ",
-              problemStopwatch.elapsed(TimeUnit.SECONDS), imageCount));
+          String.format("Problem created in %d seconds with arguments: "),
+          problemStopwatch.elapsed(TimeUnit.SECONDS));
     } catch (IOException e) {
       e.printStackTrace();
+    }
+  }
+  
+  public static int compareScore(ArrayList<String> s1, ArrayList<String> s2){
+    int scoreSim = 0;
+    int scoreDiff = 0;
+    for(String r: s1){
+      if(s2.contains(r)){
+        scoreSim++;
+      } 
+      if(!s2.contains(r)){
+        scoreDiff++;
+      }
+    }
+    
+    if(scoreDiff > scoreSim){
+      return scoreSim;
+    } else {
+      return scoreDiff;
     }
   }
 }
