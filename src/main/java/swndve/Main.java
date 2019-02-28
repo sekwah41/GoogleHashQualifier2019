887317cca579ea9d6ef4fcaf7a6d9fb9b4f4df55
James Lewis <james.lewis@jhlws.com>
2019-02-28 18:32:57 +0000

    Load problem data.

--- src/main/java/swndve/Main.java
+++ src/main/java/swndve/Main.java
@@ -10,30 +10,23 @@ public class Main {
 
   private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
   private static final Stopwatch mainStopwatch = Stopwatch.createUnstarted();
-  private static final String[] INPUT_FILES = {};
+  private static final String[] INPUT_FILES = {
+      "a_example.txt", "b_lovely_landscapes.txt", "c_memorable_moments.txt", "d_pet_pictures.txt"
+  };
 
   public static void main(String[] args) {
-
     LOGGER.setLevel(INFO);
     LOGGER.log(INFO, "Started application");
 
     for (String fileName : INPUT_FILES) {
       mainStopwatch.reset().start();
       LOGGER.log(INFO, String.format("Processing File: %s", fileName));
-
+      Problem problem = new Problem(fileName);
       LOGGER.log(
           INFO,
           String.format(
               "Processed File: %s in %d seconds",
               fileName, mainStopwatch.elapsed(TimeUnit.SECONDS)));
     }
-  }
-
-  public int[] convertToInt(String[] array) {
-    int[] intArray = new int[array.length];
-    for (int i = 0; i < array.length; i++) {
-      intArray[i] = Integer.parseInt(array[i]);
-    }
-    return intArray;
   }
 }
--- src/main/java/swndve/Problem.java
+++ src/main/java/swndve/Problem.java
@@ -6,39 +6,47 @@ import com.google.common.base.Stopwatch;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.SortedSet;
+import java.util.TreeSet;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
 public class Problem {
 
-  private static final Logger LOGGER = Logger.getLogger(Problem.class.getName());
-  private static final Stopwatch problemStopwatch = Stopwatch.createUnstarted();
+  private final Logger LOGGER = Logger.getLogger(Problem.class.getName());
+  private final Stopwatch problemStopwatch = Stopwatch.createUnstarted();
+
+  private int imageCount;
+  private List<Image> images;
+  private SortedSet<String> tags;
 
   public Problem() {
   }
 
   public Problem(String fileName) {
 
-    String line = null;
+    String line;
     try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
       problemStopwatch.reset().start();
       line = reader.readLine();
       String[] arguments = line.split(" ");
-      // TODO: Parse whole problem arguments here
-
+      imageCount = Integer.parseInt(arguments[0]);
+      images = new ArrayList<>(imageCount);
+      tags = new TreeSet<>();
       int idx = 0;
       while ((line = reader.readLine()) != null) {
-        arguments = line.split(" ");
-        // TODO: parse individual lines here
-
-        idx++;
+        Image image = Image.fromString(idx++, line);
+        tags.addAll(image.getTags());
+        images.add(image);
       }
 
-      // TODO: Log problem details here
       LOGGER.log(
           INFO,
-          String.format("Problem created in %d seconds with arguments: "),
-          problemStopwatch.elapsed(TimeUnit.SECONDS));
+          String.format(
+              "Problem created in %d seconds with arguments: Images %d ",
+              problemStopwatch.elapsed(TimeUnit.SECONDS), imageCount));
     } catch (IOException e) {
       e.printStackTrace();
     }
